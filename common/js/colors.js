688f1f6d9f1b0979dbfda1d1a1672ddddc4d5bd8
Gabriel Florit <gabrielflorit@gmail.com>
2015-03-17 18:46:21 -0400

    add utils

---
+++ common/js/colors.js
@@ -0,0 +1,21 @@
+module.exports = {
+	secondary: [
+		'#bf6151',
+		'#90b1c0',
+		'#c2cca1',
+		'#fdcd80',
+		'#faba7d',
+		'#ccb4c8',
+		'#b5997d'
+	],
+	tertiary: [
+		'#cad2d7',
+		'#dfc3b6',
+		'#c3d5de',
+		'#cfd7c8',
+		'#eeead0',
+		'#eee0d7',
+		'#dcdae5',
+		'#e3e1d6'
+	]
+};
